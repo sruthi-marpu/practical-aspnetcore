8f28fa69f49a095b10514f55a49dfe89c8430095
Dody Gunawinata <dodyg@silverkeytech.com>
2024-06-23 18:50:13 +0300

    Add antiforgery

--- projects/htmx/hx-include/Program.cs
+++ projects/htmx/hx-include/Program.cs
@@ -1,9 +1,18 @@
 using Htmx;
+using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Mvc;
 
-var app = WebApplication.Create();
-app.MapGet("/", () =>
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddAntiforgery();
+var app = builder.Build();
+
+app.UseAntiforgery();
+
+app.MapGet("/", (HttpContext context, [FromServices] IAntiforgery anti) =>
 {
-    var html = """
+    var token = anti.GetAndStoreTokens(context);
+
+    var html = $$"""
         <!DOCTYPE html>
         <html>
             <head>
@@ -12,6 +21,7 @@ app.MapGet("/", () =>
                         cursor:pointer;
                     }
                 </style>
+                <meta name="htmx-config" content='{ "antiForgery": {"headerName" : "{{ token.HeaderName}}", "requestToken" : "{{token.RequestToken }}" } }'>
             </head>
             <body>
             <h1>Passing parameters to all HTTP verbs via hx-include targeting an input form</h1>
@@ -28,13 +38,43 @@ app.MapGet("/", () =>
                 <input type="text" name="Name" id="Name"/> <br/>
 
                 <script src="https://unpkg.com/htmx.org@2.0.0" integrity="sha384-wS5l5IKJBvK6sPTKa2WZ1js3d947pvWXbPJ1OmWfEuxLgeHcEbjUUA5i9V5ZkpCw" crossorigin="anonymous"></script>
+                <script>
+                    document.addEventListener("htmx:configRequest", (evt) => {
+                        let httpVerb = evt.detail.verb.toUpperCase();
+                        if (httpVerb === 'GET') return;
+                        
+                        let antiForgery = htmx.config.antiForgery;
+                        if (antiForgery) {
+                            // already specified on form, short circuit
+                            if (evt.detail.parameters[antiForgery.formFieldName])
+                                return;
+                            
+                            if (antiForgery.headerName) {
+                                evt.detail.headers[antiForgery.headerName]
+                                    = antiForgery.requestToken;
+                            } else {
+                                evt.detail.parameters[antiForgery.formFieldName]
+                                    = antiForgery.requestToken;
+                            }
+                        }
+                    });
+                </script>
             </body>
         </html>
     """;
     return Results.Content(html, "text/html");
 });
 
-app.MapGet("/htmx/", (HttpRequest request) =>
+var htmx = app.MapGroup("/htmx").AddEndpointFilter(async (context, next) =>
+{
+    if (context.HttpContext.Request.Method == "GET")
+        return await next(context);
+
+    await context.HttpContext.RequestServices.GetRequiredService<IAntiforgery>()!.ValidateRequestAsync(context.HttpContext);
+    return await next(context);
+});
+
+htmx.MapGet("/", (HttpRequest request) =>
 {
     if (request.IsHtmx() is false)
         return Results.Content("");
@@ -42,7 +82,7 @@ app.MapGet("/htmx/", (HttpRequest request) =>
     return Results.Content($"GET => {DateTime.UtcNow} + {request.Query["Name"]}");
 });
 
-app.MapPost("/htmx/", (HttpRequest request) =>
+htmx.MapPost("/", (HttpRequest request) =>
 {
     if (request.IsHtmx() is false)
         return Results.Content("");
@@ -50,7 +90,7 @@ app.MapPost("/htmx/", (HttpRequest request) =>
     return Results.Content($"POST => {DateTime.UtcNow} + {request.Form["Name"]}");
 });
 
-app.MapDelete("/htmx/", (HttpRequest request) =>
+htmx.MapDelete("/", (HttpRequest request) =>
 {
     if (request.IsHtmx() is false)
         return Results.Content("");
@@ -58,7 +98,7 @@ app.MapDelete("/htmx/", (HttpRequest request) =>
     return Results.Content($"DELETE => {DateTime.UtcNow} + {request.Query["Name"]}");
 });
 
-app.MapPut("/htmx/", (HttpRequest request) =>
+htmx.MapPut("/", (HttpRequest request) =>
 {
     if (request.IsHtmx() is false)
         return Results.Content("");
@@ -66,7 +106,7 @@ app.MapPut("/htmx/", (HttpRequest request) =>
     return Results.Content($"PUT => {DateTime.UtcNow} + {request.Form["Name"]}");
 });
 
-app.MapPatch("/htmx/", (HttpRequest request) =>
+htmx.MapPatch("/", (HttpRequest request) =>
 {
     if (request.IsHtmx() is false)
         return Results.Content("");
