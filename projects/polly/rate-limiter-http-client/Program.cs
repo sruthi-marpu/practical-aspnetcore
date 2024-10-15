2c721cb150e9a9827fb443a26c2b87d9cbffeadd
Dody Gunawinata <dodyg@silverkeytech.com>
2024-10-15 10:38:08 +0300

    Improve retry strategy

--- projects/polly/rate-limiter-http-client/Program.cs
+++ projects/polly/rate-limiter-http-client/Program.cs
@@ -13,18 +13,39 @@ services.AddHttpClient("concurrency-http")
 .ConfigureHttpClient((sp, client) =>
 {
     client.Timeout = TimeSpan.FromSeconds(10);
-}).AddResilienceHandler("concurrency-http-policy", (builder,c) =>
+}).AddResilienceHandler("concurrency-http-policy", (builder, c) =>
 {
     builder
         .AddConcurrencyLimiter(permitLimit: 100, queueLimit: 50)
         .AddRetry(new RetryStrategyOptions<HttpResponseMessage>
         {
             MaxRetryAttempts = 3,
-            Delay = TimeSpan.FromSeconds(1),
+            DelayGenerator = static args =>
+            {
+                var delay = args.AttemptNumber switch
+                {
+                    0 => TimeSpan.Zero,
+                    1 => TimeSpan.FromSeconds(1),
+                    _ => TimeSpan.FromSeconds(5)
+                };
+
+                // This example uses a synchronous delay generator,
+                // but the API also supports asynchronous implementations.
+                return new ValueTask<TimeSpan?>(delay);
+            },
+            OnRetry = args =>
+            {
+                var logger = c.ServiceProvider.GetService<ILogger>();
+                logger.LogError("OnRetry, Attempt: {0}", args.AttemptNumber);
+                Console.WriteLine("OnRetry, Attempt: {0}", args.AttemptNumber);
+
+                // Event handlers can be asynchronous; here, we return an empty ValueTask.
+                return default;
+            },
             BackoffType = DelayBackoffType.Constant
         }).Build();
 });
 
-var app  = builder.Build();
+var app = builder.Build();
 app.MapRazorPages();
 app.Run();
